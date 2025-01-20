f2acc072810a4fdd214a48b0c1291dd96dd3aa66
Song Jimin <songwlals050328@gmail.com>
2025-01-20 10:45:34 +0900

    Spring Security Config 추가

---
+++ src/main/java/kmu/likelion/homepage/common/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package kmu.likelion.homepage.common.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Value("${spring.jwt.secretKey}")
+    private String secretKey;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions().disable()) // h2 테스트용
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new JwtTokenFilter(memberService, secretKey), UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(auth -> auth
+                        // Swagger 관련 URL을 허용, member 관련 URL을 허용
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/api/member/login",
+                                "/api/member/signup",
+                                "/api/member/find-account",
+                                "/api/member/send-verification",
+                                "/api/member/logout",
+                                "/h2-console"
+                        ).permitAll()
+
+                        // 모든 요청에 대해서 인증을 요구
+                        .anyRequest().authenticated())
+                .build();
+}
