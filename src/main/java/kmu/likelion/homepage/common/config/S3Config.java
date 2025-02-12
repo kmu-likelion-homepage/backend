8f14a4c0ee12afb854031ec56b02a6596ec10feb
Song Jimin <songwlals050328@gmail.com>
2025-02-12 17:15:59 +0900

    feature: s3 config 작성

---
+++ src/main/java/kmu/likelion/homepage/common/config/S3Config.java
@@ -0,0 +1,34 @@
+package kmu.likelion.homepage.common.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Component
+public class S3Config {
+
+    @Value("${aws.credentials.access-key}")
+    private String access_key;
+
+    @Value("${aws.credentials.secret-key}")
+    private String secret_key;
+
+    @Getter
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
+    @Value("${aws.credentials.region}")
+    private String region;
+
+    public S3Client s3Client(){
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(access_key, secret_key)))
+                .build();
+    }
+
+}
